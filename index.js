55b9ec6a62159be4e3259b5677dd17f4c2a6fc6e
osama <osama.w.hafez@gmail.com>
2022-01-01 09:49:23 -0500

    set up a simple environment to start

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 const https = require("https");
+require("dotenv").config();
 const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
-const lesson = require("./routes/lesson");
 
-var port = 3001;
+var port = process.env.BACKEND_PORT;
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(async (req, res, next) => {
-    console.log(`\nEndpoint Hit: ${req.originalUrl}\n`);
-    next();
+  console.log(`\nEndpoint Hit: ${req.originalUrl}\n`);
+  next();
 });
 
-app.use("/api", lesson);
-
 if (process.env.NODE_ENV == "production") {
-    // This sets the options for https so that it finds the ssl certificates
-    var privateKey = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
-    );
-    var certificate = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
-    );
-    var chain = fs.readFileSync(
-        "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
-    );
-    const httpsOptions = {
-        cert: certificate,
-        key: privateKey,
-        ca: chain,
-    };
+  // This sets the options for https so that it finds the ssl certificates
+  var privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/privkey.pem"
+  );
+  var certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/cert.pem"
+  );
+  var chain = fs.readFileSync(
+    "/etc/letsencrypt/live/offlinequran.org-0001/fullchain.pem"
+  );
+  const httpsOptions = {
+    cert: certificate,
+    key: privateKey,
+    ca: chain,
+  };
 
-    var httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
-        console.log("Serving on https");
-    });
+  https.createServer(httpsOptions, app).listen(port, () => {
+    console.log("Serving on https");
+  });
 } else if (process.env.NODE_ENV == "development") {
-    app.listen(port, () => {
-        console.log("Listening on port " + port);
-    });
+  app.listen(port, () => {
+    console.log("Listening on port " + port);
+  });
 }
