42940a27860322d2d5edeb9f19acebe0b69e6ca3
osama <osama.w.hafez@gmail.com>
2022-01-01 10:02:21 -0500

    admin service doesn't need any models

--- docs/run.sh
+++ docs/run.sh
@@ -1,4 +1,4 @@
-npx swagger-inline "../models/*.js" "../routes/*.js" --base oasBase.json --format .json > temp.json
+npx swagger-inline "../routes/*.js" --base oasBase.json --format .json > temp.json
 python3 formatOAS.py temp.json
 rm temp.json
 redoc-cli bundle oas.json --options.theme.colors.primary.main=brown
